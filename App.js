18cfdc47b9e42bdcd39da5d93de10e8bfab3d5b4
Dariusz Okoński <darek200180@gmail.com>
2026-02-05 20:22:04 +0100

    add basic layout

--- App.js
+++ App.js
@@ -8,7 +8,7 @@ export default function App() {
         <Button title='Add Goal' />
       </View>
 
-      <View>
+      <View style={styles.goalsContainer}>
         <Text>List of goals....</Text>
       </View>
     </View>
@@ -17,18 +17,30 @@ export default function App() {
 
 const styles = StyleSheet.create({
   appContainer: {
-    padding: 50,
+    flex: 1,
+    paddingTop: 50,
+    paddingHorizontal: 16,
     backgroundColor: 'yellow',
   },
   inputContainer: {
+    flex: 1,
     flexDirection: 'row',
     justifyContent: 'space-between',
+    alignItems: 'center',
+    marginBottom: 24,
+    borderBottomWidth: 1,
+    borderBottomColor: '#cccccc',
+    backgroundColor: 'blue',
   },
   textInput: {
     borderWidth: 1,
     borderColor: '#cccccc',
-    width: '80%',
+    width: '70%',
     marginRight: 8,
     padding: 8,
+  },
+  goalsContainer: {
+    flex: 5,
+    backgroundColor: 'green',
   },
 });
--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 import { registerRootComponent } from 'expo';
 
 import App from './App';
-import FlexBox from './FlexBox';
 
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
 // It also ensures that whether you load the app in Expo Go or in a native build,
 // the environment is set up appropriately
-registerRootComponent(FlexBox);
+registerRootComponent(App);
