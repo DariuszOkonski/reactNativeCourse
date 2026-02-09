a753902357fc8046ddd30c3c802d7c55021d14f9
Dariusz Okoński <darek200180@gmail.com>
2026-02-09 19:52:10 +0100

    add GoalInput component

---
+++ components/GoalInput.js
@@ -0,0 +1,23 @@
+import { StyleSheet, View, Text } from 'react-native';
+
+function GoalInput({ itemData }) {
+  return (
+    <View style={styles.goalItem}>
+      <Text style={styles.goalText}>{itemData.item.text}</Text>
+    </View>
+  );
+}
+
+export default GoalInput;
+
+const styles = StyleSheet.create({
+  goalItem: {
+    margin: 8,
+    padding: 8,
+    borderRadius: 6,
+    backgroundColor: '#5e0acc',
+  },
+  goalText: {
+    color: '#fff',
+  },
+});
